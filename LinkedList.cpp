37edddb37cb6c8575c8b1aa5fa6d4575d89da1a5
Jono Diver <s3707275@student.rmit.edu.au>
2019-05-07 11:34:28 +1000

    Update LinkedList.cpp
    initial implementation of linked list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -9,3 +9,39 @@ LinkedList::LinkedList() {
 
 LinkedList::~LinkedList() {
 }
+
+int size(){
+  int length = 0;
+  Node currentNode = head;
+  // empty list
+  if (curentNode == nullptr){
+    return length;
+  }
+  else if (curentNode.getNext() == nullpt){
+    length++;
+    return length;
+  }
+  else{
+    while()
+  }
+}
+
+void addFront(Tile* newTile){
+
+}
+
+void addBack(Tile* newTile){
+
+}
+
+Node* get(int i){
+
+}
+
+boolean search(Tile* currentTile){
+
+}
+
+void remove(){
+
+}
