e64d6e23d2098a6d699d01d7db3d8b1b1d9815ab
danielongRMIT <49238571+danielongRMIT@users.noreply.github.com>
2019-05-21 14:38:57 +1000

    Update qwirkle.cpp

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -3,25 +3,30 @@
 #include "Player.h"
 
 #include <iostream>
-#include <fstream>
 #include <regex>
 #include <vector>
+#include <string>
 #include <algorithm>
 
-#define EXIT_SUCCESS    0
-#define COL_INDEX       "    0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25\n"
-#define HASH_ROW        "   -------------------------------------------------------------------------------\n"
-#define BOARD_SIZE      26
+#define EXIT_SUCCESS          0
+#define COL_INDEX             "    0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25\n"
+#define HASH_ROW              "   -------------------------------------------------------------------------------\n"
+#define BOARD_SIZE            26
+#define TOTAL_TILES           6
+#define TWO_OF_EACH_IN_BAG    2
 
 /* game variables */
-std::vector<Player*> players = {nullptr};
+std::vector<Player*> players;
 Tile* board[BOARD_SIZE][BOARD_SIZE] = {{nullptr}};
+LinkedList* bag;
+Player* currentPlayer;
 
 void printMainMenu(char* input);
 void runMenu(char* input);
 void newPlayers();
-void newGame();
 void printBoard();
+void newGame();
+void firstMove();
 
 int main(void) {
 
@@ -47,57 +52,51 @@ void runMenu(char* input){
   if(*input == '1'){
     std::cout << "Starting a New Game\n" << std::endl;
     newPlayers();
-    // newGame();
-    // while !gameOver
-    // 1. The name of the current player
-    // 2. The scores of both players
-    // 3. The state of the board
-    // 4. The tiles in the current player’s hand
-    // 5. The user prompt
-    // printBoard();
-    std::cout << "Let's Play!" << std::endl;
+    std::cout << "Let's Play!\n\n";
+    newGame();
+    firstMove();
   }
   else if(*input == '2'){
-    std::cout << "Enter the filename from which load a game" << std::endl;
-    // User enters relative path
-    std::cin >> input;
-    ifstream myFile (input);
-    int count = = 0;
-    std::string line;
-    std::vector<std::string> playerdetails;
-    if (myFile.is_open()) {
-      while (getline (myFile.line) {
-        playerdetails.push_back(line);
-        count++;
-      }
-    }
-    else {
-      std::cout << "File does not exist"
-    }
-    // std::string name = "";
-    std::regex nameRegex("[A-Z]+");
-    std::regex intRegex("^\d+$")
-    std::regex handRegex("[A-Z](\d)(1,1)")
-    if (std::regex_match(playerdetails(0), nameRegex) {}
-    if (std::regex_match(playerdetails(1), intRegex) {}
-    for (int i = 0; i < playerdetails(2).length(); ++i) {
-    }
-    for (int i = 0; i < n; ++i) {
-      if
-    }
-    if (std::regex_match(playerdetails(3), nameRegex) {}
-    if (std::regex_match(playerdetails(4), intRegex) {}
-    // Check hand
-    // Check BOARD
-    // Check Bag
-    // Current Player Name
+   std::cout << "Enter the filename from which load a game" << std::endl;
+   // User enters relative path
+   std::cin >> input;
+   ifstream myFile (input);
+   int count = = 0;
+   std::string line;
+   std::vector<std::string> playerdetails;
+   if (myFile.is_open()) {
+     while (getline (myFile.line) {
+       playerdetails.push_back(line);
+       count++;
+     }
+   }
+   else {
+     std::cout << "File does not exist"
+   }
+   // std::string name = "";
+   std::regex nameRegex("[A-Z]+");
+   std::regex intRegex("^\d+$")
+   std::regex handRegex("[A-Z](\d)(1,1)")
+   if (std::regex_match(playerdetails(0), nameRegex) {}
+   if (std::regex_match(playerdetails(1), intRegex) {}
+   for (int i = 0; i < playerdetails(2).length(); ++i) {
+   }
+   for (int i = 0; i < n; ++i) {
+     if
+   }
+   if (std::regex_match(playerdetails(3), nameRegex) {}
+   if (std::regex_match(playerdetails(4), intRegex) {}
+   // Check hand
+   // Check BOARD
+   // Check Bag
+   // Current Player Name
 
-    // 1. Check that the file exists.
-    // 2. Check that the format of the file is correct
+   // 1. Check that the file exists.
+   // 2. Check that the format of the file is correct
 
-    // If validation checks out
-    std::cout << "Qwirkle game successfully loaded" << std::endl;
-    //gameplay continue from here
+   // If validation checks out
+   std::cout << "Qwirkle game successfully loaded" << std::endl;
+   //gameplay continue from here
   }
   else if(*input == '3'){
     std::cout << "\n\n------------------------------\n" << std::endl;
@@ -143,7 +142,7 @@ void newPlayers(){
 
 void printBoard(){
   char rowLetter = 'A';
-  std::cout << COL_INDEX << HASH_ROW;
+  std::cout << "\n" << COL_INDEX << HASH_ROW;
   for(int i = 0; i < BOARD_SIZE; i++){
     std::cout << rowLetter << "  |";
     rowLetter++;
@@ -152,15 +151,66 @@ void printBoard(){
         std::cout << "  |";
       }
       else{
-        std::cout << board[i][j] << "|";
+        std::cout << board[i][j]->colour << board[i][j]->shape << "|";
       }
     }
-    std::cout << std::endl;
+    std::cout << "\n";
   }
+  std::cout << "\n";
 }
 
 void newGame(){
-  // create bag
-  // give each player 6 tiles
-  //
+  // create bag of 72 tiles, 2 of each tile
+  bag = new LinkedList();
+  for(int i = 1; i <= TOTAL_TILES;  i++){
+    for(int j = 0; j < TWO_OF_EACH_IN_BAG;  j++){
+      bag->addFront(new Tile('R', i));
+      bag->addFront(new Tile('O', i));
+      bag->addFront(new Tile('Y', i));
+      bag->addFront(new Tile('G', i));
+      bag->addFront(new Tile('B', i));
+      bag->addFront(new Tile('P', i));
+    }
+  }
+  // bag->display();
+  // std::cout << "number of tiles in bag : " << bag->size() << std::endl;
+  // std::cout << "player vector contains:";
+
+  /* gives each player a new random hand */
+  for (std::vector<Player*>::iterator it = players.begin(); it != players.end(); ++it){
+    // std::cout << ' ' << (*it)->name << "'s hand: ";
+    (*it)->giveNewHand(*bag);
+    // (*it)->displayHand();
+  }
+  // std::cout << "number of tiles in bag : " << bag->size() << std::endl;
+}
+
+void firstMove(){
+  currentPlayer = players.front();
+  std::cout << currentPlayer->name << ", it's your turn\n";
+  for (std::vector<Player*>::iterator it = players.begin(); it != players.end(); ++it){
+    std::cout << "Score for " << (*it)->name << ": " << (*it)->score << std::endl;
+  }
+  printBoard();
+  std::cout << "Your hand is\n";
+  currentPlayer->displayHand();
+  std::string tileToPlace = " ";
+  std::regex tileRegex("place [ROYGBP]{1}[1-6]{1}");
+  while(!std::regex_match(tileToPlace, tileRegex)){
+    std::cout << "Choose tile to place at N13\n> ";
+    std::cin >> tileToPlace;
+  }
+  // printBoard();
+}
+
+void gameplay(){
+     // while !gameOver
+     //   1. The name of the current player
+     //   2. The scores of both players
+     //   3. The state of the board
+     //   4. The tiles in the current player’s hand
+     //   5. The user prompt
+     //   6. Execute user prompt
+     //   7. gameplay()
+
 }
